77ac2a9196e2367d632a5f0b1eac7bb255bf7ead
utkarshrai9 <utkarsh.rai@students.mq.edu.au>
2019-08-26 02:36:32 +1000

    Extracted Labels from the repos

--- get_repos.py
+++ get_repos.py
@@ -3,10 +3,11 @@ import json
 from datetime import datetime
 import pprint
 
-list_pub_repo = 'https://api.github.com/repositories?since=364'
+list_pub_repo = 'https://api.github.com/repositories'
 
-r = requests.get(list_pub_repo)
-
+secret_key = "Enter your own secret_key"
+headers = {'Authorization': 'token %s' % secret_key}
+r = requests.get(list_pub_repo, headers=headers)
 # last_repo = r.json()[-2]
 # print(json.dumps(last_repo, sort_keys = True, indent = 4))
 # last_updated = last_repo.get("id")
@@ -16,17 +17,32 @@ r = requests.get(list_pub_repo)
 # 	dt = datetime.strptime("2011-01-26T19:14:43Z", "%Y-%m-%dT%H:%M:%SZ")
 	# print(dt)
 for repo in r.json():
-	print(r)
-	repo_url = repo.get("owner").get("url")
+	# print(r.json())
+	repo_url = repo.get("url")
 	if repo_url is not None:
-		repo_get_request_data = requests.get(repo_url)
+		repo_get_request_data = requests.get(repo_url, headers=headers)
 		repo_data = repo_get_request_data.json()
 		repo_updated = repo_data.get("updated_at")
 		# print(repo_updated)
+		if repo_updated is None:
+			print("Looking XXX...      ", end="", flush=True)
+			continue
 		dt = datetime.strptime(repo_updated, "%Y-%m-%dT%H:%M:%SZ")
-		# print(dt)
 		dt_year = dt.year
-		print(dt_year)
+		# print(dt_year)
+		if dt_year>=datetime.now().year-1:
+			# print(repo_url)
+			labels_url = repo.get("labels_url")
+			if labels_url is not None:
+				labels_url = labels_url.split("{")[0]
+				labels = requests.get(labels_url, headers=headers).json()
+				if len(labels):
+					print([i['name'] for i in labels])
+			else:
+				print("Looking XXX...      ", end="", flush=True)
+
+
+
 
 
 # repoURL = r.json.repo("url")
